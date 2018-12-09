b6fa9b1921179c74d8ca64463a32a63803bd4b53
Minter27 <mohammad.alshbool2002@gmail.com>
2018-12-09 14:01:58 +0200

    Done

--- Flask and Jinja/application.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    someArray = ["/<your pathway>"]
-
-    return render_template("index.html", slides=someArray)
