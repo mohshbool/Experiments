3194376cda9d92a973b9870bdf5fd9be6ada5d92
Mohammad Shbool <mohammad.alshbool2002@gmail.com>
2019-04-18 19:33:19 +0300

    changed variable declarations
    to es6 syntax

--- linear-distance-between-two-coordinates-js/script.js
+++ linear-distance-between-two-coordinates-js/script.js
@@ -3,16 +3,18 @@ function degreesToRadians(degrees) {
 }
 
 function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
-  var earthRadiusKm = 6371;
+  const EARTH_RADIUS_IN_KM = 6371;
 
-  var dLat = degreesToRadians(lat2-lat1);
-  var dLon = degreesToRadians(lon2-lon1);
+  let dLat = degreesToRadians(lat2-lat1);
+  let dLon = degreesToRadians(lon2-lon1);
 
   lat1 = degreesToRadians(lat1);
   lat2 = degreesToRadians(lat2);
 
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+  let a = Math.sin(dLat/2) * Math.sin(dLat/2) +
           Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  
   return earthRadiusKm * c;
 }
